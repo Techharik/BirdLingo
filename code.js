1aecd534564f881e686977713c0636393fd47a90
Hari Prasath <khariprasath30@gmail.com>
2026-02-19 11:29:12 +0530

    basic text node iteranton

--- code.js
+++ code.js
@@ -1,34 +1,28 @@
-
 figma.showUI(__html__, { width: 400, height: 300 });
-
-const selection = figma.currentPage.selection;
-
+var selection = figma.currentPage.selection;
 if (selection.length === 0) {
     figma.notify("Please select a frame.");
     figma.closePlugin();
 }
-
 // Only take first selected node
-const selectedNode = selection[0];
-
+var selectedNode = selection[0];
 if (selectedNode.type !== "FRAME") {
     figma.notify("Please select a FRAME.");
     figma.closePlugin();
 }
-
-function getTextNodes(node: SceneNode, texts: string[] = []) {
+function getTextNodes(node, texts) {
+    if (texts === void 0) { texts = []; }
     if (node.type === "TEXT") {
         texts.push(node.characters);
     }
     if ("children" in node) {
-        for (const child of node.children) {
+        for (var _i = 0, _a = node.children; _i < _a.length; _i++) {
+            var child = _a[_i];
             getTextNodes(child, texts);
         }
     }
     return texts;
 }
-
-const extractedTexts = getTextNodes(selectedNode);
+var extractedTexts = getTextNodes(selectedNode);
 figma.ui.postMessage({ type: "TEXT_RESULT", data: extractedTexts });
-
 console.log("Extracted Texts:", extractedTexts);
